b2777a8397d6dc8c9c6d748be707a7c53fa441a9
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-06-16 01:14:59 -0700

    connected when2meet event scraping

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,31 @@
 <script setup lang="ts">
 const route = useRoute();
-console.log("path: ", route.fullPath);
 
-const { data, error } = await useFetch("/api/create-schej-event", {
-  method: "POST",
-});
-console.log(data.value, error.value);
-
-const count = ref(0);
 const id = computed(() => {
   return Object.keys(route.query)[0];
+});
+
+const loading = ref(true);
+const timeTook = ref(0);
+
+onMounted(async () => {
+  loading.value = true;
+  const start = new Date().getTime();
+  const data = await $fetch("/api/create-schej-event", {
+    method: "POST",
+    body: { href: route.fullPath },
+  });
+  const end = new Date().getTime();
+  loading.value = false;
+  console.log(data);
+
+  timeTook.value = (end - start) / 1000;
 });
 </script>
 
 <template>
   <h1>Query: {{ route.query }}</h1>
   <h2>ID: {{ id }}</h2>
-  <button @click="count++">Increment: {{ count }}</button>
+  <div v-if="loading">Loading............</div>
+  <div v-else>Done in {{ timeTook }}s!</div>
 </template>
--- server/api/create-schej-event.post.ts
+++ server/api/create-schej-event.post.ts
@@ -1,6 +1,99 @@
+import puppeteerCore from "puppeteer-core";
+import puppeteer from "puppeteer";
+import chromium from "@sparticuz/chromium";
+
+/** Get the correct browser depending on if we're running in production
+ *  Source: https://gist.github.com/kettanaito/56861aff96e6debc575d522dd03e5725
+ */
+async function getBrowser() {
+  if (process.env.VERCEL_ENV === "production") {
+    const executablePath = await chromium.executablePath();
+
+    const browser = await puppeteerCore.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath,
+      headless: chromium.headless,
+    });
+    return browser;
+  } else {
+    const browser = await puppeteer.launch();
+    return browser;
+  }
+}
+
 export default defineEventHandler(async (event) => {
-  console.log("event", event);
+  const body = await readBody(event);
+  const href = body.href;
+
+  const browser = await getBrowser();
+  const page = await browser.newPage();
+
+  await page.goto(`https://when2meet.com${href}`);
+
+  //
+  // Get event details (dates, duration)
+  //
+  const groupGridElement = await page.$("#GroupGridSlots");
+
+  // @ts-ignore
+  const data = await page.evaluate((groupGrid: HTMLElement) => {
+    const duration = groupGrid.children.length / 4;
+    const dates = [];
+    for (const col of groupGrid.children[0].children) {
+      dates.push(new Date(parseInt(col.getAttribute("data-time") + "000")));
+    }
+    return { dates: JSON.stringify(dates), duration };
+  }, groupGridElement);
+
+  // Fix dates
+  data.dates = JSON.parse(data.dates).map((d: string) => new Date(d));
+
+  //
+  // Get event name
+  //
+  const eventNameElement = await page.$("#NewEventNameDiv");
+  // @ts-ignore
+  const eventName = await page.evaluate((eventNameEl: HTMLElement) => {
+    return eventNameEl.innerHTML.split("<br>")[0].trim();
+  }, eventNameElement);
+  data.name = eventName;
+
+  //
+  // Get map mapping name to available times
+  //
+  const availability: { [key: string]: number[] } = {};
+  const availableElement = await page.$("#Available");
+  for (const date of data.dates) {
+    for (
+      let i = 0, curDate = date;
+      i < data.duration * 4;
+      i++, curDate.setMinutes(curDate.getMinutes() + 15)
+    ) {
+      const groupTime = await page.$(`#GroupTime${curDate.getTime() / 1000}`);
+
+      // @ts-ignore
+      await groupTime.hover();
+
+      // @ts-ignore
+      const available = await page.evaluate((availableEl: HTMLElement) => {
+        const available = availableEl.innerHTML.split("<br>");
+        available.splice(-1);
+        return available;
+      }, availableElement);
+
+      for (const name of available) {
+        if (name in availability) {
+          availability[name].push(curDate.getTime());
+        } else {
+          availability[name] = [curDate.getTime()];
+        }
+      }
+    }
+  }
+  data.availability = availability;
+
   return {
-    event,
+    ...data,
   };
 });
