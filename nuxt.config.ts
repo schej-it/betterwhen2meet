c4fbc77a38fefa8f77075688defbadb08498c2a2
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-06-18 00:48:37 -0700

    implemented posthog

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    public: {
+      posthogPublicKey: "phc_pz4u2FxpKnE15iVzI7fRYe9QClKm0tI9xVtkCVioThk",
+      posthogHost: "https://betterwhen2meet.com/ingest",
+    },
+  },
+  routeRules: {
+    "/ingest/static/**": { proxy: "https://us-assets.i.posthog.com/static/**" },
+    "/ingest/**": { proxy: "https://us.i.posthog.com/**" },
+  },
 });
